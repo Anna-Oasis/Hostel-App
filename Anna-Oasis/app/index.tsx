d88adbad923ef83ca6e5f772d532203947215432
Rishi-Kumar <152687290+Rishi-kumar18@users.noreply.github.com>
2025-05-21 11:36:41 +0530

    dummy first page

--- Anna-Oasis/app/index.tsx
+++ Anna-Oasis/app/index.tsx
@@ -1,17 +1,14 @@
 import { Text, View } from "react-native";
 import { Button, ButtonText } from "@/components/ui/button";
 import TestForm from "@/components/TestForm";
-import AdmissionForm from "@/components/AdmissionForm";
 import React from "react";
 import { Link, router } from "expo-router";
 
-
 export default function Index() {
- 
   return (
     <>
       <View className="flex m-4 gap-4">
-        <Button onPress={()=> router.push('/Login')} >
+        <Button onPress={() => router.push("/Login")}>
           <ButtonText>Student</ButtonText>
         </Button>
         <Button>
