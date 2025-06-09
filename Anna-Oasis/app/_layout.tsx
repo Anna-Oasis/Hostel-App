d9bac91b42aef09fde6f2ba4d4309e0178a7214e
Rishi-Kumar <rk18102004@gmail.com>
2025-06-09 19:56:50 +0530

    RC Routing done

--- Anna-Oasis/app/_layout.tsx
+++ Anna-Oasis/app/_layout.tsx
@@ -19,6 +19,14 @@ export default function Layout() {
         <Stack.Screen name="User/Student/LeaveForm/index" options={{ headerShown: false }} />
         <Stack.Screen name="User/Student/SummerVacation/index" options={{ headerShown: false }} />
         <Stack.Screen name="RC/index" options={{ headerShown: false }} />
+        <Stack.Screen name="RC/RoomAllocation/index" options={{ headerShown: false }} />
+        <Stack.Screen name="RC/StudentVerification/index" options={{ headerShown: false }} />
+        <Stack.Screen name="RC/StudentVerification/LeaveForm/index" options={{ headerShown: false }} />
+        <Stack.Screen name="RC/StudentVerification/Grievances/index" options={{ headerShown: false }} />
+        <Stack.Screen name="RC/StudentVerification/SummerVacation/index" options={{ headerShown: false }} />
+        <Stack.Screen name="RC/StudentVerification/VacatingHostel/index" options={{ headerShown: false }} />
+        <Stack.Screen name="RC/Attendance/index" options={{ headerShown: false }} />
+        <Stack.Screen name="RC/ApplyForLeave/index" options={{ headerShown: false }} />
         <Stack.Screen name="Manager/index" options={{ headerShown: false }} />
         <Stack.Screen name="ExecutiveWarden/index" options={{ headerShown: false }} />
         <Stack.Screen name="DeputyWarden/index" options={{ headerShown: false }} />
