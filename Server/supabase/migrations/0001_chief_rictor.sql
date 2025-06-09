9a0c79368eb5142904b91aff38a98990f6f3b44d
Shevaaniarun <shevaaniarun@gmail.com>
2025-06-09 17:59:28 +0530

    Added some fields in Student Model

---
+++ Server/supabase/migrations/0001_chief_rictor.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "student" ADD COLUMN "gender" varchar(15) NOT NULL;--> statement-breakpoint
+ALTER TABLE "student" ADD COLUMN "passport_url" text;--> statement-breakpoint
+ALTER TABLE "student" ADD COLUMN "aadhaar_url" text;--> statement-breakpoint
+ALTER TABLE "student" ADD COLUMN "admission_slip_url" text;
