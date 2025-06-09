f93b4688cebea2f86a68e444d4b3227cbe8c52e3
Suhaib Sharieff <sharieffsuhaib@gmail.com>
2025-06-09 13:20:14 +0530

    details route complete

---
+++ Server/routes/detailsRoute.ts
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import { validateJWT } from "../middleware/jwt";
+import { upload } from "../middleware/multer";
+import { getDetailsByRollNo,createStudentDetails,updateStudentDetails } from "../controllers/detailsController";
+const detailsRouter = Router();
+
+detailsRouter.get("/:rollNo",(req,res,next) => {
+    getDetailsByRollNo(req, res, next).catch(next);
+})
+
+detailsRouter.post(
+  "/",
+  upload.fields([
+    { name: "passportPhotoUrl", maxCount: 1 },
+    { name: "studentSignatureUrl", maxCount: 1 },
+    { name: "parentGuardianSignatureUrl", maxCount: 1 },
+  ]),
+  (req, res, next) => {
+    // console.log("Hello from details route");
+    // console.log("Request body:", req.body);
+    createStudentDetails(req, res, next).catch(next);
+  }
+);
+
+detailsRouter.post("/test", (req, res, next) => {
+  console.log("🔥 Reached test POST route before Multer");
+  res.send("OK");
+});
+
+
+
+
+detailsRouter.put(
+  "/:rollNo",
+  upload.fields([
+    { name: "passportPhotoUrl", maxCount: 1 },
+    { name: "studentSignatureUrl", maxCount: 1 },
+    { name: "parentGuardianSignatureUrl", maxCount: 1 },
+  ]),
+  (req, res, next) => {
+    updateStudentDetails(req, res, next).catch(next);
+  }
+);
+
+
+export default detailsRouter;
--- Server/server.ts
+++ Server/server.ts
@@ -12,8 +12,12 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 app.use("/api", routes);
-
+app.use("/", authRoutes);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     const local = res.send;
@@ -24,7 +28,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     next();
 });
 
-app.use("/", authRoutes);
+
 
 app.get("/health", (req: Request, res: Response) => {
     res.status(200).json({ status: "ok" });
