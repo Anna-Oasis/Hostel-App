50ab87d466783b837c9111234e5a7ce3986a25cb
161-Keshav <ppg.gnanakeshav@gmail.com>
2025-06-10 17:58:27 +0530

    manager admissions approval done

--- Server/routes/managerRoutes.ts
+++ Server/routes/managerRoutes.ts
@@ -1,10 +1,28 @@
-import {Router} from 'express';
+import { Router, Request, Response } from 'express';
+import { db } from '../config/dbConnection';  
+import { admissionApprovalsModel } from '../models/admissionApprovals';
+import { admissionModel } from '../models/admissionModel';
+import { userModel } from '../models/userModel';
+import { desc } from 'drizzle-orm';
 
+const router: Router = Router();
 
-const managerRouter = Router();
+router.get("/admissions/approval", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const approvals = await db
+      .select()
+      .from(admissionApprovalsModel)
+      .orderBy(desc(admissionApprovalsModel.timestamp));
 
-managerRouter.get("/", (req, res) => {
-  res.send("👋 Welcome to the Manager API!");
+    res.status(200);
+    res.json(approvals);
+  } catch (error) {
+
+    res.status(500);
+    res.json({ error: 'Failed to fetch' });
+  }
 });
 
-export default managerRouter;
+
+
+export default router;
