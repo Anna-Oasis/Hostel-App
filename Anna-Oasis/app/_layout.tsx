134712e2150e7538a9662b02e81679993d740c25
Rishi-Kumar <rk18102004@gmail.com>
2025-06-11 19:00:44 +0530

    feat(routes): add role-based routes and update login navigation
    - Added routes for Manager: payment verifications, grievances, caution deposit
    - Added routes for Deputy Warden: admission, leave, summer vacation, vacating hostel verifications, grievances, RC management, attendance reports
    - Added route for Executive Warden: admission verification
    - Updated login logic to route users to(User/Student)
    - Updated authUtlis.ts to login to (User/Student)

--- Anna-Oasis/app/_layout.tsx
+++ Anna-Oasis/app/_layout.tsx
@@ -1,35 +1,16 @@
 import { Stack } from "expo-router";
-
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function Layout() {
   return (
     <GluestackUIProvider mode="light">
-      <Stack>
-        <Stack.Screen name="Login/index" options={{ headerShown: false }} />
-        <Stack.Screen name="Signup/index" options={{ headerShown: false }} />
-        <Stack.Screen name="User/index" options={{ headerShown: false }} />
-        <Stack.Screen name="User/Research/index" options={{ headerShown: false }} />
-        <Stack.Screen name="User/Student/index" options={{ headerShown: false }} />
-        <Stack.Screen name="User/Student/Admission/index" options={{ headerShown: false }} />
-        <Stack.Screen name="User/Student/Details/index" options={{ headerShown: false }} />
-        <Stack.Screen name="User/Student/Grievances/index" options={{ headerShown: false }} />
-        <Stack.Screen name="User/Student/HostelVacation/index" options={{ headerShown: false }} />
-        <Stack.Screen name="User/Student/LeaveForm/index" options={{ headerShown: false }} />
-        <Stack.Screen name="User/Student/SummerVacation/index" options={{ headerShown: false }} />
-        <Stack.Screen name="RC/index" options={{ headerShown: false }} />
-        <Stack.Screen name="RC/RoomAllocation/index" options={{ headerShown: false }} />
-        <Stack.Screen name="RC/StudentVerification/index" options={{ headerShown: false }} />
-        <Stack.Screen name="RC/StudentVerification/LeaveForm/index" options={{ headerShown: false }} />
-        <Stack.Screen name="RC/StudentVerification/Grievances/index" options={{ headerShown: false }} />
-        <Stack.Screen name="RC/StudentVerification/SummerVacation/index" options={{ headerShown: false }} />
-        <Stack.Screen name="RC/StudentVerification/VacatingHostel/index" options={{ headerShown: false }} />
-        <Stack.Screen name="RC/Attendance/index" options={{ headerShown: false }} />
-        <Stack.Screen name="RC/ApplyForLeave/index" options={{ headerShown: false }} />
-        <Stack.Screen name="Manager/index" options={{ headerShown: false }} />
-        <Stack.Screen name="ExecutiveWarden/index" options={{ headerShown: false }} />
-        <Stack.Screen name="DeputyWarden/index" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        {/* 
+          You only need to add <Stack.Screen /> here 
+          if you want to override options for a specific route.
+          Otherwise, Expo Router will auto-discover all routes in /app.
+        */}
       </Stack>
     </GluestackUIProvider>
   );
