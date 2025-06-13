26668aa7d54ceeca7468cb3f68dfe7da999e40a8
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-06-13 19:23:41 +0530

    [UI] Admission form done

--- Anna-Oasis/constants/admission.ts
+++ Anna-Oasis/constants/admission.ts
@@ -1,4 +1,3 @@
-
 const admissionCategories = [
   { label: "NRI", value: "NRI" },
   { label: "CIWGC", value: "CIWGC" },
@@ -7,17 +6,16 @@ const admissionCategories = [
   { label: "SIDS", value: "SIDS" },
 ];
 
-
 const initialValues = {
-  admissionCategory: "",
-  previousResident: "",
-  messPreference: "",
+  admissionCategory: undefined,
+  previousResident: undefined,
+  messPreference: undefined,
   studentAgreed: "",
   parentAgreed: "",
   submissionDate: "",
   transactionId: "",
   academicYear: "",
-  hostelBlock: ""
+  hostelBlock: undefined
 };
 
 const hostelBlocks = [
@@ -25,8 +23,20 @@ const hostelBlocks = [
   { label: "Lavendar (Girls)", value: "Lavendar" },
 ];
 
+const messPreferences = [
+  { label: "Veg", value: "Veg" },
+  { label: "Non-Veg", value: "Non-Veg" },
+];
+
+const previousResidentOptions = [
+  { label: "Yes", value: "Yes" },
+  { label: "No", value: "No" },
+];
+
 export {
   admissionCategories,
   initialValues,
   hostelBlocks,
+  messPreferences,
+  previousResidentOptions,
 };
