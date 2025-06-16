c0640b5173bb499e49708542b7d914c44634de68
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-06-16 21:18:07 +0530

    [FIX] import fix deputy

--- Server/routes/index.ts
+++ Server/routes/index.ts
@@ -3,7 +3,6 @@ import authRouter from "./authRoutes";
 import detailsRouter from "./detailsRoute";
 import studentRouter from "./studentRoutes";
 import managerRouter from "./managerRoutes";
-import deputyRouter from './deputyWardenRoutes'
 import rcRouter from "./rcRoutes";
 import deputyWardenRouter from "./deputyWardenRoutes";
 // import { generatePdf, PDFData } from "../utils/pdfGenerator"; // Uncomment if you want to use the PDF generation route
@@ -14,7 +13,6 @@ routes.use("/", authRouter);
 routes.use("/api/details", detailsRouter);
 routes.use("/api/student/", studentRouter);
 routes.use("/api/manager/", managerRouter);
-routes.use("/api/deputy_warden/", deputyRouter);
 routes.use("/api/resident_counsellor/",rcRouter);
 routes.use("/api/deputy_warden/",deputyWardenRouter);
 
