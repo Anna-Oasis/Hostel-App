dcfe3302b78e2a01361a0a9f02f29fd07f5aa9f7
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-06-18 19:39:09 +0530

    [FIX] fixing inconsistent routes

--- Server/controllers/grievanceController.ts
+++ Server/controllers/grievanceController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { createGrievance, getGrievancesByRollNumber } from "../services/grievanceService";
+import { AuthRequest } from "../types/roles";
+import { getGrievancesForManager, resolveGrievanceByManager } from "../services/grievanceService";
 import httpStatus from "http-status";
 import { AppError } from "../utils/AppError";
 
 
-export const createGrievanceController = async (req: Request, res: Response) => {
+export const createGrievanceController = async (req: AuthRequest, res: Response) => {
   const data = req.body;
   const result = await createGrievance(data);
 
@@ -15,7 +17,7 @@ export const createGrievanceController = async (req: Request, res: Response) => 
   });
 };
 
-export const getGrievancesByRollNumberController = async (req: Request, res: Response): Promise<void>=> {
+export const getGrievancesByRollNumberController = async (req: AuthRequest, res: Response): Promise<void>=> {
   const rollNumber = req.params.roll_number;
   
   const result = await getGrievancesByRollNumber(rollNumber);
@@ -30,3 +32,42 @@ export const getGrievancesByRollNumberController = async (req: Request, res: Res
     message: "Grievances fetched successfully",
   });
 };
+
+export const getGreivancesForManagerFromController=async (req:AuthRequest,res:Response)=>
+{
+    const data=await getGrievancesForManager();
+
+    if(data.length===0)
+    {
+      throw AppError("No Greivances Found",httpStatus.NOT_FOUND);
+    }
+
+    res.status(httpStatus.OK)
+    .json(
+      {
+        success:true,
+        data:data,
+        message:"Greivances Fetched Successfully"
+      }
+    )
+}
+
+export const resolveGrievanceFromController = async (req: AuthRequest,res:Response)=>
+{
+    const greivanceId=Number(req.params.grievance_id);
+    const data=await resolveGrievanceByManager(greivanceId);
+
+    if(data.length===0)
+    {
+      throw AppError("Greivance with Greivance ID not Found",httpStatus.NOT_FOUND);
+    }
+
+    res.status(httpStatus.OK)
+    .json(
+      {
+        success:true,
+        data:data,
+        message:"Greivance Resolved Successfully"
+      }
+    );
+}
--- Server/routes/managerRoutes.ts
+++ Server/routes/managerRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { getAdmissionWaitingForApprovalController, updateApprovalStatusController,fetchAdmissionsApprovedByUser } from '../controllers/admissionController';
 import errorWrapper from "../middleware/errorWrapper";
-import {getGreivancesForManagerFromController,resolveGrievanceFromController} from '../controllers/managerController';
+import { resolveGrievanceFromController, getGreivancesForManagerFromController } from '../controllers/grievanceController';
 import { authenticateUser,hasRole } from '../middleware/rbacMiddleware';
 
 const managerRouter = Router();
@@ -13,10 +13,10 @@ managerRouter.put("/admissions/:admission_id", authenticateUser,hasRole(['manage
 managerRouter.get("/admissions/approvals",authenticateUser,hasRole(['manager']),errorWrapper(fetchAdmissionsApprovedByUser));
 
 //PUT-manager/grievance
-managerRouter.put("/grievance/:grievance_id",errorWrapper(resolveGrievanceFromController));
+managerRouter.put("/grievance/:grievance_id",authenticateUser,hasRole(['manager']),errorWrapper(resolveGrievanceFromController));
 
 //GET-manager/grievance
-managerRouter.get("/grievance",errorWrapper(getGreivancesForManagerFromController));
+managerRouter.get("/grievance",authenticateUser,hasRole(['manager']),errorWrapper(getGreivancesForManagerFromController));
 
 export default managerRouter;
 
