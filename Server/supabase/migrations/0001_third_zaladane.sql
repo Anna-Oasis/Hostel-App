566c0621f4c1ca6fb6037f16874731d6b2df57d5
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-06-18 14:46:02 +0530

    Stop tracking migrations folder

--- Server/supabase/migrations/0001_third_zaladane.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "room" (
-	"room_number" integer NOT NULL,
-	"hostel_block" varchar(20) NOT NULL,
-	"academic_year" varchar(9) NOT NULL,
-	"floor" integer NOT NULL,
-	"rollNo" varchar(20)[],
-	CONSTRAINT "room_room_number_hostel_block_academic_year_pk" PRIMARY KEY("room_number","hostel_block","academic_year")
-);
