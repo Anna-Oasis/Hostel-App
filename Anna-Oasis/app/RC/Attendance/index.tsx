c32ea46aaec23b36426babd66f4cb3b720e77a1a
Gogul11 <gogul0811@gmail.com>
2025-06-19 22:27:43 +0530

    attendance form

--- Anna-Oasis/app/RC/Attendance/index.tsx
+++ Anna-Oasis/app/RC/Attendance/index.tsx
@@ -1,8 +1,76 @@
-import { View, Text } from "react-native";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Checkbox, CheckboxIcon, CheckboxIndicator } from "@/components/ui/checkbox";
+import { CheckIcon } from "@/components/ui/icon";
+import { Table, TableHead, TableHeader, TableRow, TableBody, TableData, TableFooter } from "@/components/ui/table";
+import { View, Text, ScrollView } from "react-native";
+import { markPresent, sampleStudents } from "@/utils/RcAttendenceUtils";
+
 export default function AttendancePage() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Attendance (Coming Soon)</Text>
-    </View>
+    <ScrollView>
+      <View className="flex-1 justify-center items-center m-4 rounded-2xl">
+        <Text className="text-2xl m-2 font-bold">RC Name Attendance</Text>
+        <ScrollView horizontal>
+          <Table className="w-full">
+            <TableHeader>
+              <TableRow>
+                {["Roll No", "Name", "Floor", "Block", "Present"].map((item, index) => (
+                    <>
+                      <TableHead 
+                        className={`w-[150px] overflow-scroll ${index !== 4 && 'border-r'} text-center`}
+                        key={index}>
+                        {item}
+                      </TableHead>
+                    </>
+                  ))}
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {sampleStudents.map((item, rowIndex) => (
+                <TableRow key={rowIndex}>
+                  {Object.entries(item).slice(0, -1).map(([key, value], colIndex) => (
+                    <TableData
+                      key={colIndex}
+                      className="w-[150px] overflow-scroll border-r text-center font-light"
+                    >
+                      {value?.toString()}
+                    </TableData>
+                  ))}
+                  <TableData className="w-[150px] overflow-scroll  text-center">
+                    <Checkbox 
+                        value=""
+                        size="md" 
+                        isInvalid={false} 
+                        isDisabled={false}
+                        onChange={() => markPresent(rowIndex)}
+                    >
+                      <CheckboxIndicator>
+                        <CheckboxIcon as={CheckIcon}/>
+                      </CheckboxIndicator>
+                    </Checkbox>
+                </TableData>
+                </TableRow>
+              ))}
+        
+            </TableBody>
+
+            <TableFooter>
+              <TableRow>
+                <TableData>
+                  <Button onPress={() => console.log(sampleStudents)}>
+                    <ButtonText>
+                      Submit
+                    </ButtonText>
+                  </Button>
+                </TableData>
+              </TableRow>
+            </TableFooter>
+          </Table>
+
+        </ScrollView>
+      </View>
+    </ScrollView>
+
   );
 }
---
+++ Anna-Oasis/utils/RcAttendenceUtils.ts
@@ -0,0 +1,43 @@
+
+ const hostelBlock = {
+  BOYS: "Flora",
+  GIRLS: "Lavender"
+}
+
+type student = {
+    rollNo : number;
+    name : string;
+    floor : number;
+    block : typeof hostelBlock[keyof typeof hostelBlock];
+    present ?: boolean;
+}
+
+
+//sample recodrs from backend
+let sampleStudents: student[] = [
+  { rollNo: 2023115001, name: "Aarav Kumar", floor: 1, block: "Flora" },
+  { rollNo: 2023115002, name: "Meera Iyer", floor: 2, block: "Lavender" },
+  { rollNo: 2023115003, name: "Rohan Das", floor: 3, block: "Flora" },
+  { rollNo: 2023115004, name: "Diya Reddy", floor: 1, block: "Lavender" },
+  { rollNo: 2023115005, name: "Karan Verma", floor: 2, block: "Flora" },
+  { rollNo: 2023115006, name: "Sneha Nair", floor: 3, block: "Lavender" },
+  { rollNo: 2023115007, name: "Vikram Singh", floor: 1, block: "Flora" },
+  { rollNo: 2023115008, name: "Pooja Sharma", floor: 2, block: "Lavender" },
+  { rollNo: 2023115009, name: "Aditya Menon", floor: 3, block: "Flora" },
+  { rollNo: 2023115010, name: "Nisha Patel", floor: 1, block: "Lavender" },
+];
+
+//Adding the extra field present to the values from backend by default false
+sampleStudents.forEach(item => {
+  item.present = false;
+});
+
+//Marking the student present or absent
+const markPresent = (index : number) => {
+    const updated = [...sampleStudents]
+    updated[index].present = !updated[index].present
+    sampleStudents = updated
+}
+
+export {sampleStudents, markPresent}
+
