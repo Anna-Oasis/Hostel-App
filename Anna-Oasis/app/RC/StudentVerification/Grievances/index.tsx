28ae6021952833aa4db3a20e0620d9a6facac75d
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-06-23 18:57:19 +0530

    [ADD] Integrated RC Grievances

--- Anna-Oasis/app/RC/StudentVerification/Grievances/index.tsx
+++ Anna-Oasis/app/RC/StudentVerification/Grievances/index.tsx
@@ -1,8 +1,44 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+import { useEffect, useState } from "react";
+import { getAllRCGrievances } from "@/utils/rc/rcGrievanceApi";
+import ApprovalCard from "@/components/ApprovalCard";
+import { getGrievanceBadgeStatus } from "@/utils/getBadgeStatus";
+
 export default function GrievancesPage() {
+  const [grievances, setGrievances] = useState<any[]>([]);
+
+  useEffect(() => {
+    getAllRCGrievances()
+      .then((data) => {
+        setGrievances(data || []);
+      })
+      .catch((error) => {
+        console.log("Error fetching RC grievances:", error);
+      });
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Grievances verification (Coming Soon)</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} className="p-4">
+      {grievances.length === 0 ? (
+        <View className="flex-1 justify-center items-center">
+          <Text className="text-base text-gray-700">Grievances verification (Coming Soon)</Text>
+        </View>
+      ) : (
+        grievances.map((item, idx) => (
+          <ApprovalCard
+            key={item.grievances.id || idx}
+            title={item.grievances.subject}
+            subTitle={`By ${item.student.name} (${item.student.rollNo})`}
+            badge={getGrievanceBadgeStatus(item.grievances.status)}
+            onApprove={() => { /* dummy approve */ }}
+            onDecline={() => { /* dummy decline */ }}
+            data={{
+              ...item.grievances,
+              ...item.student,
+            }}
+          />
+        ))
+      )}
+    </ScrollView>
   );
 }
