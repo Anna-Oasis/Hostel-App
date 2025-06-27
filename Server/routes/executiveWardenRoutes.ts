3d64ac1ec808300e3c8e1eb9bb36e0c8ab033ec5
Varun-Karthik-T <varunkarthikceg@gmail.com>
2025-06-28 01:14:53 +0530

    updated the new admission logic
    declaration_id is hardcoded for now. Have to update after declaration logic.

--- Server/routes/executiveWardenRoutes.ts
+++ Server/routes/executiveWardenRoutes.ts
@@ -6,6 +6,7 @@ import {
   fetchAdmissionsApprovedByUser,
   fetchAdmissionWaitingForApprovalController,
   updateApprovalStatusByWardenController,
+  allocateRoomController
 } from "../controllers/admissionController";
 import errorWrapper from "../middleware/errorWrapper";
 import { authenticateUser, hasRole } from "../middleware/rbacMiddleware";
@@ -42,6 +43,13 @@ executiveWardenRouter.get(
   errorWrapper(fetchAdmissionsApprovedByUser)
 );
 
+executiveWardenRouter.put(
+  "/admissions/room/:admission_id",
+  authenticateUser,
+  hasRole(["executiveWarden"]),
+  errorWrapper(allocateRoomController)
+);
+
 // Room details route
 executiveWardenRouter.get(
   "/rooms",
@@ -62,4 +70,6 @@ executiveWardenRouter.put(
   hasRole(["executiveWarden"]),
   errorWrapper(updateLeaveStatusForRC)
 );
+
+
 export default executiveWardenRouter;
--- Server/validation/warden.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const wardenDecisionSchema = z.object({
-  approve: z.boolean(),
-  comment: z.string().optional(),
-});
-
-
