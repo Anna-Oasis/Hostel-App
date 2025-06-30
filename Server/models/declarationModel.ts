154ca27bdf66e27630124655f948b5330884dccb
Salai-Kowshikan <salaikowshikan8743@gmail.com>
2025-06-30 12:52:04 +0530

    [FIX] limit on declaration

--- Server/models/declarationModel.ts
+++ Server/models/declarationModel.ts
@@ -10,7 +10,7 @@ export const declarationModel = pgTable("declaration", {
   id: serial("id").primaryKey(),
   timestamp: timestamp("timestamp").defaultNow().notNull(),
   type: declarationEnum("type").notNull(),
-  declarations: varchar("declarations", { length: 1000 }).array().notNull(),
+  declarations: varchar("declarations").array().notNull(),
 });
 
 export type Declaration = typeof declarationModel.$inferSelect;
