2adc4e7b3798833bed0cd17caa419d26af48f2b3
Gnana Keshav <ppg.gnanakeshav@gmail.com>
2026-05-02 15:05:35 +0530

    [ADD]-room structure creation

---
+++ Server/services/roomStructureAllocationService.ts
@@ -0,0 +1,27 @@
+import { roomModel } from "../models/roomModel";
+import { db } from "../config/dbConnection";
+
+type RoomInsert = {
+  roomNumber: number;
+  hostelBlock: string;
+  academicYear: string;
+  floor: number;
+  rollNo: string[] | null;
+};
+
+export const roomStructureAllocationService = async (
+  roomsToInsert: RoomInsert[]
+) => {
+  const batchSize = 100;
+
+  await db.transaction(async (bt) => {
+    for (let i = 0; i < roomsToInsert.length; i += batchSize) {
+      const batch = roomsToInsert.slice(i, i + batchSize);
+      await bt.insert(roomModel).values(batch);
+    }
+  });
+
+  return {
+    count: roomsToInsert.length,
+  };
+};
