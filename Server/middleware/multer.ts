23042bde83f6efa8cbe118936653a0f4807f8d02
@Gogul <148183352+Gogul11@users.noreply.github.com>
2026-06-17 13:43:13 +0530

    Add support for JPEG image file types in multer

--- Server/middleware/multer.ts
+++ Server/middleware/multer.ts
@@ -37,6 +37,7 @@ export const upload = multer({
     );
     if (
       file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/jpg" ||
       file.mimetype === "image/png" ||
       file.mimetype === "application/pdf"
     ) {
