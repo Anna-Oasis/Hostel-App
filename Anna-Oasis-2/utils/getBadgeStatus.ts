71b649b8cd698a571cb8d8731d425f70aadf8d15
Shankar-147 <sankarramsu@gmail.com>
2026-07-18 14:27:36 +0530

    feat(grievance): add pending and history tabs for RC

--- Anna-Oasis-2/utils/getBadgeStatus.ts
+++ Anna-Oasis-2/utils/getBadgeStatus.ts
@@ -1,5 +1,6 @@
   import { badgeStatus } from '@/components/ApprovalCard'
-  
+  import { GRIEVANCE_STATUS } from '@/constants/grievanceStatus'
+
   export const getAdmissionBadgeStatus = (status: string) => {
       if (status === "3") return badgeStatus.Approved
       if (status === "-1") return badgeStatus.Rejected
@@ -7,8 +8,8 @@
   }
 
    export const getGrievanceBadgeStatus = (status: string) => {
-      if (status === "1") return badgeStatus.Approved
-      if (status === "-1") return badgeStatus.Rejected
+      if (status === GRIEVANCE_STATUS.RC) return badgeStatus.Approved
+      if (status === GRIEVANCE_STATUS.DECLINED) return badgeStatus.Rejected
     return badgeStatus.Pending
   }
 
