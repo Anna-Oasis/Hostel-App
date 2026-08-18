d9636fc83aaeea96e08120c4184acfc2858a6a3e
Gogul11 <gogul0811@gmail.com>
2026-08-18 20:24:35 +0530

    Only backend

--- Anna-Oasis/constants/validations/grievanceValidation.ts
+++
@@ -1,7 +0,0 @@
-import * as Yup from "yup";
-
-export const grievanceValidationSchema = Yup.object().shape({
-  subject: Yup.string().required("Subject is required"),
-  description: Yup.string().required("Description is required"),
-  grievance_type: Yup.string().required("Category is required"),
-});
