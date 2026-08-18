4986b24972a10cf142c3b399fb38507b20b60d3f
Gogul11 <gogul0811@gmail.com>
2026-08-18 20:35:33 +0530

    Only web

--- Server/validation/cautionDeposit.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const cautionDepositSchema = z.object({
-  accountHolderName: z.string(),
-  accountNumber: z.string(),
-  bankName: z.string(),
-  addressOfTheBank: z.string(),
-  IFSCode: z.string(),
-});
